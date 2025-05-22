3ffb9464ba4db1590182adeada76ced330157121
Alexandre <alexmorimitsu@gmail.com>
2025-05-22 11:48:53 -0300

    Fix

--- main/app.py
+++ main/app.py
@@ -227,11 +227,11 @@ fig = f_figure_scatter_plot(df, _columns=['x', 'y'], _selected_custom_data=[], b
                             xrange = background_ranges['x'], yrange=background_ranges['y'])
 
 #_columns_paralelas_coordenadas = ['Layer_A', 'Layer_B', 'Layer_C', 'Layer_D', 'Layer_E', 'Layer_F', 'Layer_G']
-if df.shape[1] < 30: #v0.2
-    if 'D2' in df:
-        _columns_paralelas_coordenadas = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
-    else:
-        _columns_paralelas_coordenadas = ['D1', 'x2', 'y2', 'D4', 'x3', 'y3', 'D7']
+#if df.shape[1] < 30: #v0.2
+if 'D2' in df:
+    _columns_paralelas_coordenadas = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
+else:
+    _columns_paralelas_coordenadas = ['D1', 'x2', 'y2', 'D4', 'x3', 'y3', 'D7']
 #else: #v0.3
 #    _columns_paralelas_coordenadas = ['D1', 'D4', 'D7', 'Area (pxl)', 'circularity', 'Elongation', 'Mean Intensity']
 
